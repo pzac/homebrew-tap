7c0feb36888446a7df47e48d6eca5b03816ae5e2
Paolo Zaccagnini <paolo@pzac.net>
2026-03-22 11:49:32 +0100

    Blamer v0.3.0

--- Formula/blamer.rb
+++ Formula/blamer.rb
@@ -1,8 +1,8 @@
 class Blamer < Formula
     desc "CLI tool to investigate a file history via git blame"
     homepage "https://github.com/pzac/blamer"
-    url "https://github.com/pzac/blamer/releases/download/v0.2.0/blamer-v0.2.0.tar.gz"
-    sha256 "4a1135608470a9085e6c257de9248576def237e3b6dadb3e0a2c76024d6ff967"
+    url "https://github.com/pzac/blamer/releases/download/v0.3.0/blamer-v0.3.0.tar.gz"
+    sha256 "fab49cb2ab371de9f430df16119db7191402276cdb6b72cae3b78a8b17c98e73"
 
     depends_on "rust" => :build
 
